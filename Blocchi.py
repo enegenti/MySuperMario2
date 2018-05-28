7f1ff580c490c7238fd31c4b557758fbdf3e00fb
Elia <elia.ferrari@students.fhnw.ch>
2018-05-28 14:42:23 +0200

    terza modifica

--- Blocchi.py
+++ Blocchi.py
@@ -4,13 +4,45 @@ import Variabili as v
 
 # blocchi #
 class Brick():
-    def __init__(self, x, y):
-        self.sprite = arcade.Sprite("images/block.png")
-        self.sprite.center_x = x
-        self.sprite.center_y = y
+    def __init__(self, contents=None, x, y):
+        self.image = arcade.Sprite("images/block.png")
+        self.image.center_x = x
+        self.image.center_y = y
         self.contents = contents
+        self.state = v.RESTING
+
 
     def update (self):
+        if self.state == v.RESTING:
+            self.resting()
+        elif self.state == v.BUMPED:
+            self.bumped()
+        elif self.state == v.OPENED:
+            self.opened()
 
 
-    def get_image(self, x, y, width, height): # estrae l'immagine dal file
+    def setup_contents(self):
+        if self.contents == '6coins':
+            self.coin_total = 6
+        else:
+            self.coin_total = 0
+
+
+    def resting(self): #quando non si muove
+        if self.contents == '6coins':
+            if self.coin_total == 0:
+                self.state == v.OPENED
+
+
+    def bumped(self): #quando viene colpito
+        if self.contents == '6coins':
+            if self.coin_total == 0:
+                self.state = v.OPENED
+            else:
+                self.state = v.RESTING
+        else:
+            self.state = v.RESTING
+
+
+    def opened(self): #quando viene aperto
+        self.image = arcade.Sprite("images/coin.png")
